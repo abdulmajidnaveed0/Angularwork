b07e32c42f8b5da4ca56294b1a84d7f3140f103c
abdulmajidnaveed0 <abdulmajidnaveed0@gmail.com>
2024-02-18 14:17:11 -0500

    update app.module

--- Project2/jobportal/src/app/app.module.ts
+++ Project2/jobportal/src/app/app.module.ts
@@ -11,19 +11,52 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 
+import { environment } from 'src/environments/environment';  // firebase
+import {AngularFireModule} from '@angular/fire/compat';   // firebase
+import {AngularFireDatabaseModule, AngularFireDatabase} from '@angular/fire/compat/database';
+import { JobslistComponent } from './components/jobslist/jobslist.component';
+import { JobdetailsComponent } from './components/jobdetails/jobdetails.component';
+import { AddjobComponent } from './components/addjob/addjob.component';
+import { BasicApiComponent } from './components/basic-api/basic-api.component';
+import { JobsbrowseComponent } from './components/jobsbrowse/jobsbrowse.component';
+import { LoginComponent } from './login/login.component';
+
+// https://www.bezkoder.com/angular-14-firebase-crud/
+// first video from Tania's channel 
+//  https://www.youtube.com/watch?v=O0uVYhRE850
+
+// first hit, this is with Firestore 
+// https://www.bezkoder.com/integrate-firebase-angular-15/
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    JobslistComponent,
+    JobdetailsComponent,
+    AddjobComponent,
+    BasicApiComponent,
+    JobsbrowseComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, ReactiveFormsModule, HttpClientModule
+    FormsModule, ReactiveFormsModule, HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // firebase
+    AngularFireDatabaseModule, // firebase
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+https://firebase.google.com/docs/web/setup#add-sdk-and-initialize
+https://firebase.google.com/docs/database/web/start
+https://www.bezkoder.com/angular-14-firebase-crud/
+https://betterprogramming.pub/how-to-create-and-configure-a-firebase-and-angular-project-9305c40ee308
+
+*/
