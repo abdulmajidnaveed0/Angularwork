04e3eebc32f668ed25b7a101b3a4592661494f21
abdulmajidnaveed0 <abdulmajidnaveed0@gmail.com>
2024-02-18 14:06:08 -0500

    add firebase config

--- Project2/jobportal/src/environments/environment.ts
+++ Project2/jobportal/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCgIE-8nigc_nFfMicCP7nfQsD3ilNb5XA",
+    authDomain: "angular-9bbee.firebaseapp.com",
+    projectId: "angular-9bbee",
+    storageBucket: "angular-9bbee.appspot.com",
+    messagingSenderId: "999925519047",
+    appId: "1:999925519047:web:27eed3b4a9b36e9a72a4e2",
+
+    databaseURL:"https://angular-9bbee-default-rtdb.firebaseio.com/"
+  },  
 };
 
 /*
