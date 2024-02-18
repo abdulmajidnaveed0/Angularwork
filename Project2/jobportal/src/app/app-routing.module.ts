59dd84e3e012749d8f219fae70c1b816c53aa228
abdulmajidnaveed0 <abdulmajidnaveed0@gmail.com>
2024-02-18 14:15:10 -0500

    update routes

--- Project2/jobportal/src/app/app-routing.module.ts
+++ Project2/jobportal/src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+
+//import { AuthGuardService } from './services/guard.service';
+import {LoginAuthService} from './services/loginauth.service';
+import { LoginguardService } from './services/loginguard.service';
 
 //const routes: Routes = [];
 //const routes: Routes = [];
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'admin', component: AdminComponent},
+  {path: 'admin', component: AdminComponent, canActivate:
+   [LoginguardService]},
+  {path: 'login', component: LoginComponent}
 ];
 
 
